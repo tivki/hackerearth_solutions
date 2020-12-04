73fbc0826d770eaf69adc45f5df65382d93588f4
tivki <74445433+tivki@users.noreply.github.com>
2020-12-04 12:03:13 +0530

    Update Split_houses.py

--- Split_houses.py
+++ Split_houses.py
@@ -2,11 +2,11 @@
 
 n=int(input())
 
-for i>=1 and i<=10:
+if i>=1 and i<=10:
     st=input()
-    if "HH" in st:
-        print("NO")
-    else:
-        st.replace(".","B")
-        print("YES")
-        print(st)
+if "HH" in st:
+    print("NO")
+else:
+    st.replace(".","B")
+    print("YES")
+    print(st)
